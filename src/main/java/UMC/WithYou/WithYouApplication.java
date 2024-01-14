ce2426793f905d65ba304b0736dfee9916434459
DryRains <dryrain_@naver.com>
2024-01-14 20:56:53 +0900

    first commit

---
+++ src/main/java/UMC/WithYou/WithYouApplication.java
@@ -0,0 +1,13 @@
+package UMC.WithYou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithYouApplication.class, args);
+	}
+
+}
