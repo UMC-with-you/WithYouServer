cd38201d8de6ba9989ab388114c7565c02a11dd4
DryRains <dryrain_@naver.com>
2024-01-20 20:45:35 +0900

    [CHORE]#3 Setup basic directories

---
+++ src/main/java/UMC/WithYou/common/enums/status/MemberStatus.java
@@ -0,0 +1,5 @@
+package UMC.WithYou.common.enums.status;
+
+public enum MemberStatus {
+    ACTIVE, INACTIVE
+}
---
+++ src/main/java/UMC/WithYou/common/enums/type/SocialType.java
@@ -0,0 +1,5 @@
+package UMC.WithYou.common.enums.type;
+
+public enum SocialType {
+    KAKAO, NAVER, GOOGLE, FACEBOOK
+}
