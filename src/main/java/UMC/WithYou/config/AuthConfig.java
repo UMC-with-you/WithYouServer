5419447d09546994034670e47545324c75bf4508
hojinida <opqrs1103@gmail.com>
2024-01-21 13:56:54 +0900

    [FEAT]#5 Create AuthFilter for Authentication

--- src/main/java/UMC/WithYou/config/AuthConfig.java
+++ src/main/java/UMC/WithYou/config/AuthConfig.java
@@ -1,9 +1,13 @@
 package UMC.WithYou.config;
 
+import UMC.WithYou.service.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +21,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.List;
 
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class AuthConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
