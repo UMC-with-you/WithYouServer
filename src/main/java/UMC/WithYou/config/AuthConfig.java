5f6c34ec5712e43e8eb5a7668647ec92a6d0c0f0
hojinida <opqrs1103@gmail.com>
2024-01-21 12:23:41 +0900

    [FEAT]#5 Create TokenProvider for JWT Authentication

--- src/main/java/UMC/WithYou/config/AuthConfig.java
+++ src/main/java/UMC/WithYou/config/AuthConfig.java
@@ -1,4 +1,4 @@
-package UMC.WithYou.auth.config;
+package UMC.WithYou.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
