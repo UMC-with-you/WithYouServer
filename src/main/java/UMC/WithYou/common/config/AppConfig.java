d4aee9cef36f32d53389a6113f2dfa608621d187
hojinida <opqrs1103@gmail.com>
2024-01-31 01:00:00 +0900

    [FEAT]#43 Create @Member for AOP

--- src/main/java/UMC/WithYou/common/config/AppConfig.java
+++ src/main/java/UMC/WithYou/common/config/AppConfig.java
@@ -1,14 +1,27 @@
 package UMC.WithYou.common.config;
 
+import UMC.WithYou.common.resolver.MemberArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
-public class AppConfig {
+@RequiredArgsConstructor
+public class AppConfig implements WebMvcConfigurer {
+    private final MemberArgumentResolver memberArgumentResolver;
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberArgumentResolver);
     }
 }
--- src/main/java/UMC/WithYou/service/auth/UserDetailsServiceImpl.java
+++ src/main/java/UMC/WithYou/service/auth/UserDetailsServiceImpl.java
@@ -1,5 +1,8 @@
 package UMC.WithYou.service.auth;
 
+import UMC.WithYou.domain.auth.UserPrincipal;
+import UMC.WithYou.domain.member.Member;
+import UMC.WithYou.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,8 +14,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private final MemberRepository memberRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Member member = memberRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+
+        return new UserPrincipal();
     }
 }
