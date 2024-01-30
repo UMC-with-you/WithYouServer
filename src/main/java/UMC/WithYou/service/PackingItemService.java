ca7b26c162da0cd6fb12cd1bd0bb44c385833bec
Genius-PAK <pyj789123@gmail.com>
2024-01-30 16:30:42 +0900

    [FEAT]#35 add method handling NON-EXISTS exception to find packing item with id

--- src/main/java/UMC/WithYou/service/PackingItemService.java
+++ src/main/java/UMC/WithYou/service/PackingItemService.java
@@ -1,11 +1,13 @@
 package UMC.WithYou.service;
 
-import UMC.WithYou.domain.dummy.DummyMember;
+import UMC.WithYou.common.apiPayload.code.status.ErrorStatus;
+import UMC.WithYou.common.apiPayload.exception.handler.CommonErrorHandler;
 import UMC.WithYou.domain.PackingItem;
 import UMC.WithYou.domain.dummy.DummyTravel;
-import UMC.WithYou.repository.dummy.DummyMemberRepository;
+import UMC.WithYou.domain.member.Member;
 import UMC.WithYou.repository.PackingItemRepository;
 import UMC.WithYou.repository.dummy.DummyTravelRepository;
+import UMC.WithYou.repository.member.MemberRepository;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -15,35 +17,46 @@ import org.springframework.stereotype.Service;
 public class PackingItemService {
     private final PackingItemRepository packingItemRepository;
     private final DummyTravelRepository travelRepository;
-    private final DummyMemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
 
-    public PackingItemService(PackingItemRepository packingItemRepository, DummyTravelRepository travelRepository, DummyMemberRepository memberRepository){
+    public PackingItemService(PackingItemRepository packingItemRepository, DummyTravelRepository travelRepository, MemberRepository memberRepository){
         this.packingItemRepository = packingItemRepository;
         this.travelRepository = travelRepository;
         this.memberRepository = memberRepository;
     }
 
     public PackingItem addPackingItem(Long travelId, String itemName) {
-        DummyTravel travel = travelRepository.findById(travelId).get();
+        DummyTravel travel = travelRepository.findById(travelId).orElseThrow(
+                ()->new CommonErrorHandler(ErrorStatus.TRAVEL_LOG_NOT_FOUND)
+        );
+
         PackingItem packingItem =  PackingItem.createPackingItem(travel, itemName);
 
         return packingItemRepository.save(packingItem);
     }
 
     public List<PackingItem> findPackingItems(Long travelId){
+        DummyTravel travel = travelRepository.findById(travelId).orElseThrow(
+                ()->new CommonErrorHandler(ErrorStatus.TRAVEL_LOG_NOT_FOUND)
+        );
         return packingItemRepository.findByTravelId(travelId);
     }
 
-    public void deletePackingItem(Long id){
-        PackingItem packingItem = packingItemRepository.findById(id).get();
+    public void deletePackingItem(Long packingItemId){
+        PackingItem packingItem = this.findPeckingItemById(packingItemId);
         packingItemRepository.delete(packingItem);
     }
 
     public Boolean setPacker(Long packingItemId, Long packerId){
-        PackingItem packingItem = packingItemRepository.findById(packingItemId).get();
-        DummyMember packer = memberRepository.findById(packerId).get();
-        if (packerId.equals(packer.getId())){
+        PackingItem packingItem = this.findPeckingItemById(packingItemId);
+
+        Member packer = memberRepository.findById(packerId).orElseThrow(
+                ()->new CommonErrorHandler(ErrorStatus.MEMBER_NOT_FOUND)
+        );
+
+        if (packingItem.getPacker() != null && packingItem.getPacker().isSameId(packerId)){
+
             if (packingItem.getIsChecked()) {
                 packingItem.toggleCheckbox();
             }
@@ -54,11 +67,19 @@ public class PackingItemService {
     }
 
     public Boolean toggleCheckbox(Long packingItemId) {
-        PackingItem packingItem = packingItemRepository.findById(packingItemId).get();
+        PackingItem packingItem = this.findPeckingItemById(packingItemId);
 
         if (packingItem.isPackerChosen()){
             return packingItem.getIsChecked();
         }
         return packingItem.toggleCheckbox();
+    }
+
+
+
+    private PackingItem findPeckingItemById(Long packingItemId){
+        return packingItemRepository.findById(packingItemId).orElseThrow(
+                ()->new CommonErrorHandler(ErrorStatus.PACKING_ITEM_NOT_FOUND)
+        );
     }
 }
