7d8cb0d5ef3a9b309b82ffae188f95648e5bd92f
Genius-PAK <pyj789123@gmail.com>
2024-01-30 16:29:24 +0900

    [REFACTOR]#35 refine packingItem dtos

--- src/main/java/UMC/WithYou/dto/PackingItemRequest.java
+++ src/main/java/UMC/WithYou/dto/PackingItemRequest.java
@@ -1,18 +1,11 @@
 package UMC.WithYou.dto;
 
-import UMC.WithYou.domain.PackingItem;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 public class PackingItemRequest {
     @Getter
     public static class AdditionRequestDTO{
-        private Long travelId;
         private String itemName;
     }
-
-
 
 }
