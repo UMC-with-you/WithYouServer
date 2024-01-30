a84bc756b8752a8b294984930782f094f5c01968
Genius-PAK <pyj789123@gmail.com>
2024-01-30 15:25:49 +0900

    [FEAT]#19 add temporary function to identify member with virtual token number

--- src/main/java/UMC/WithYou/service/member/MemberService.java
+++ src/main/java/UMC/WithYou/service/member/MemberService.java
@@ -1,9 +1,23 @@
 package UMC.WithYou.service.member;
 
+import UMC.WithYou.common.apiPayload.code.status.ErrorStatus;
+import UMC.WithYou.common.apiPayload.exception.handler.CommonErrorHandler;
+import UMC.WithYou.domain.member.Member;
+import UMC.WithYou.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+    public Member findMemberById(Long memberId){
+        return memberRepository.findById(memberId).orElseThrow(
+                ()->new CommonErrorHandler(ErrorStatus.MEMBER_NOT_FOUND)
+        );
+    }
+    public Member findByMemberIdToken(String token){
+
+        return this.findMemberById(Long.parseLong(token));
+    }
 }
