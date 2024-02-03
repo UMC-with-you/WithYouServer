f4707aeabb128ea326ec3276e28800cffd4cb2b8
pyomin4973 <hyomin8918@naver.com>
2024-02-03 19:00:00 +0900

    refactor: state 추가

--- src/main/java/UMC/WithYou/common/config/AppConfig.java
+++ src/main/java/UMC/WithYou/common/config/AppConfig.java
@@ -1,14 +1,20 @@
 package UMC.WithYou.common.config;
 
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.List;
 
 @Configuration
-public class AppConfig {
-
+@RequiredArgsConstructor
+public class AppConfig implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
@@ -18,4 +24,5 @@ public class AppConfig {
     JPAQueryFactory jpaQueryFactory(EntityManager em) {
         return new JPAQueryFactory(em);
     }
+
 }
