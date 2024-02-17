38f3b818dcb6ae4a7bd713f0060924a3aae26d63
hojinida <opqrs1103@gmail.com>
2024-02-18 00:38:41 +0900

    [FEAT]#61 Nonce, ClientId 검증 추가

--- src/main/java/UMC/WithYou/service/auth/AppleTokenVerifier.java
+++ src/main/java/UMC/WithYou/service/auth/AppleTokenVerifier.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,32 +33,43 @@ public class AppleTokenVerifier {
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
 
-    public DecodedJWT verifyToken(String token,String nonce) throws Exception {
+    public DecodedJWT verifyToken(String token, String nonce) throws Exception {
         ApplePublicKeyResponse response = fetchApplePublicKeys();
-        ApplePublicKeyResponse.AppleKey matchingKey = findMatchingKey(token, response);
-        RSAPublicKey publicKey = generatePublicKey(matchingKey);
+        RSAPublicKey publicKey = getPublicKeyFromResponse(token, response);
 
-        DecodedJWT jwt = verifyJwtToken(token, publicKey);
+        DecodedJWT jwt = decodeAndVerifyToken(token, publicKey);
 
-        if (!verifyNonce(jwt, nonce)) {
-            throw new SecurityException("Nonce verification failed.");
-        }
-
-        if (!verifyClientId(jwt, clientId)) {
-            throw new SecurityException("Client ID verification failed.");
-        }
+        verifyNonce(jwt, nonce);
+        verifyClientId(jwt, clientId);
 
         return jwt;
     }
 
-    private boolean verifyNonce(DecodedJWT jwt, String expectedNonce) {
-        String tokenNonce = jwt.getClaim("nonce").asString();
-        return tokenNonce != null && tokenNonce.equals(expectedNonce);
+    private RSAPublicKey getPublicKeyFromResponse(String token, ApplePublicKeyResponse response) throws Exception {
+        ApplePublicKeyResponse.AppleKey matchingKey = findMatchingKey(token, response);
+        return generatePublicKey(matchingKey);
     }
 
-    private boolean verifyClientId(DecodedJWT jwt, String expectedClientId) {
-        String audience = jwt.getAudience().get(0); // ID 토큰은 보통 하나의 audience 값을 갖습니다.
-        return audience != null && audience.equals(expectedClientId);
+    private DecodedJWT decodeAndVerifyToken(String token, RSAPublicKey publicKey) {
+        Algorithm algorithm = Algorithm.RSA256(publicKey, null);
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer("https://appleid.apple.com")
+                .build();
+        return verifier.verify(token);
+    }
+
+    private void verifyNonce(DecodedJWT jwt, String expectedNonce) {
+        String tokenNonce = jwt.getClaim("nonce").asString();
+        if (tokenNonce == null || !tokenNonce.equals(expectedNonce)) {
+            throw new SecurityException("Nonce verification failed.");
+        }
+    }
+
+    private void verifyClientId(DecodedJWT jwt, String expectedClientId) {
+        String audience = jwt.getAudience().get(0);
+        if (audience == null || !audience.equals(expectedClientId)) {
+            throw new SecurityException("Client ID verification failed.");
+        }
     }
 
     private ApplePublicKeyResponse fetchApplePublicKeys() throws Exception {
@@ -67,7 +79,7 @@ public class AppleTokenVerifier {
 
     private ApplePublicKeyResponse.AppleKey findMatchingKey(String token, ApplePublicKeyResponse response) throws Exception {
         String header = new String(Base64.getUrlDecoder().decode(token.split("\\.")[0]));
-        Map<String, String> headerMap = objectMapper.readValue(header, Map.class);
+        Map<String, String> headerMap = objectMapper.readValue(header, new TypeReference<>() {});
         String kid = headerMap.get("kid");
 
         return response.getKeys().stream()
@@ -85,12 +97,5 @@ public class AppleTokenVerifier {
         RSAPublicKeySpec spec = new RSAPublicKeySpec(modulus, exponent);
         KeyFactory factory = KeyFactory.getInstance("RSA");
         return (RSAPublicKey) factory.generatePublic(spec);
-    }
-    private DecodedJWT verifyJwtToken(String token, RSAPublicKey publicKey) {
-        Algorithm algorithm = Algorithm.RSA256(publicKey, null);
-        JWTVerifier verifier = JWT.require(algorithm)
-                .withIssuer("https://appleid.apple.com")
-                .build();
-        return verifier.verify(token);
     }
 }
